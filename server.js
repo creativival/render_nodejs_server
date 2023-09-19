1c2d64bcddfbdd00d80c2117e3140bdbd3d9521b
creativival <creativival@gmail.com>
2023-09-19 16:51:22 +0900

    サーバーに初回メッセージの検証とエラー処理を追記

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const WebSocket = require('ws');
 
-let rooms = {}
+let rooms = {};
 
-const wss = new WebSocket.Server({port: 8765});
+const wss = new WebSocket.Server({ port: 8765 });
 
 wss.on('connection', function connection(ws) {
   let roomName = null;
 
   ws.on('message', function incoming(message) {
+    // 初回メッセージの検証
     if (!roomName) {
+      if (typeof message === 'string' || message.length  < 10) { // 例: 部屋名の長さ制限
+        ws.send('Invalid room name');
+        ws.close();
+        return;
+      }
       roomName = message;
       if (!rooms[roomName]) {
         rooms[roomName] = new Set();
@@ -30,12 +36,33 @@ wss.on('connection', function connection(ws) {
 
   ws.on('close', function close() {
     // Unregister client
-    if (roomName) {
+    if (roomName && rooms[roomName]) {
       rooms[roomName].delete(ws);
+      // 部屋のクリーンアップ
+      if (rooms[roomName].size === 0) {
+        delete rooms[roomName];
+      }
     }
   });
 
   ws.on('error', function error(e) {
-    console.log(`Error: ${e}`);
+    // エラーロギング
+    console.error(`WebSocket Error: ${e.message}`);
+
+    // クライアントへの通知 (詳細なエラー情報は送信しない)
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send('An error occurred. Please try again later.');
+    }
+
+    // リソースのクリーンアップ (この例では部屋からのクライアントの削除)
+    if (roomName && rooms[roomName]) {
+      rooms[roomName].delete(ws);
+      if (rooms[roomName].size === 0) {
+        delete rooms[roomName];
+      }
+    }
+
+    // アラート通知 (例: Slack、Eメールなどの通知サービスを使用)
+    // sendAlertToOpsTeam(`WebSocket Error: ${e.message}`);
   });
 });
